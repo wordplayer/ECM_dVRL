af5b79f1bd103dcc2a103a8771397a4b60fd69a0
wordplayer <soumyarajbo@gmail.com>
2020-09-25 20:57:14 -0700

    Adding install scripts

---
+++ setup.py
@@ -0,0 +1,8 @@
+from disutils.core import setup
+setup(
+    name='ECM_dVRL',
+    version='0.0.1',
+    packages=['ECM_dVRL_v01',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
